2ac2f99086cdf5e4ce316cfbea251643c38d19ee
Anurag Makineni <anurag.makineni@gmail.com>
2016-10-28 14:20:46 -0400

    added multiarray sin test

--- ros/adaptive_lighting/scripts/sin_test.py
+++ ros/adaptive_lighting/scripts/sin_test.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64MultiArray
 import math
 
-pub = rospy.Publisher('/pwm_driver/pwm', Float64, queue_size=10)
+pub = rospy.Publisher('/pwm_driver/pwm', Float64MultiArray, queue_size=10)
 rospy.init_node('sin_test')
 
 t = 0.0
 r = rospy.Rate(50)
 
 while not rospy.is_shutdown():
+    msg = Float64MultiArray()
     val = 0.5*(math.sin(2.0*t) + 1.0);
-    pub.publish(val)
+    currents = [val, -val, val, -val]
+    msg.data = currents
+    pub.publish(msg)
     t = t + 0.02;
     r.sleep()
