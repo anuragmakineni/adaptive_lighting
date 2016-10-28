35b85a0014e8b2cffa5363c216a11bf41f390294
Anurag Makineni <anurag.makineni@gmail.com>
2016-10-28 12:12:46 -0400

    added multiple LED support

--- arduino/src/pwm_driver/pwm_driver.ino
+++ arduino/src/pwm_driver/pwm_driver.ino
@@ -1,21 +1,56 @@
 // code reads an integer over serial (0-255). then sets PWM
 // to recieved value.
 
-int output_pin = 3;
-int PWM_val = 0;
+#define PIN_A 3
+#define PIN_B 9
+#define PIN_C 10
+#define PIN_D 11
+
+int PWM_A = 0;
+int PWM_B = 0;
+int PWM_C = 0;
+int PWM_D = 0;
 
 void setup() {
-  pinMode(output_pin, OUTPUT);
+  pinMode(PIN_A, OUTPUT);
+  pinMode(PIN_B, OUTPUT);
+  pinMode(PIN_C, OUTPUT);
+  pinMode(PIN_D, OUTPUT);
   Serial.begin(115200);
-  Serial.setTimeout(10);
+  Serial.setTimeout(1);
 }
 
 void loop() {
-  analogWrite(output_pin, PWM_val);
+  analogWrite(PIN_A, PWM_A);
+  analogWrite(PIN_B, PWM_B);
+  analogWrite(PIN_C, PWM_C);
+  analogWrite(PIN_D, PWM_D);
   
-  if(Serial.available() > 0)
-  {
-    PWM_val = Serial.parseInt();
-    PWM_val = constrain(PWM_val, 0, 255);
+  while (Serial.available())
+  { 
+    if (Serial.find("A"))
+    {
+      int val = Serial.parseInt();
+      PWM_A = constrain(val, 0, 255);
+    }
+    else break;
+    if (Serial.find("B"))
+    {
+      int val = Serial.parseInt();
+      PWM_B = constrain(val, 0, 255);
+    }
+    else break;
+    if (Serial.find("C"))
+    {
+      int val = Serial.parseInt();
+      PWM_C = constrain(val, 0, 255);
+    }
+    else break;
+    if (Serial.find("D"))
+    {
+      int val = Serial.parseInt();
+      PWM_D = constrain(val, 0, 255);
+    }
+    else break;
   }
 }
