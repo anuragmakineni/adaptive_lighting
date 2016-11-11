fb36bc946271339ebffe652e62d1252b1a0a315f
Anurag Makineni <anurag.makineni@gmail.com>
2016-11-10 21:02:01 -0500

    made serial faster

--- arduino/src/pwm_driver/pwm_driver.ino
+++ arduino/src/pwm_driver/pwm_driver.ino
@@ -17,40 +17,43 @@ void setup() {
   pinMode(PIN_C, OUTPUT);
   pinMode(PIN_D, OUTPUT);
   Serial.begin(115200);
-  Serial.setTimeout(1);
+  Serial.setTimeout(10);
 }
 
+String command;
 void loop() {
+  if (Serial.available())
+  {
+    char c = Serial.read();
+    
+    if (c == '\n')
+    {
+      parseCommand(command);
+      command = "";
+    }
+    else
+    {
+      command += c;
+    }
+  }
+}
+
+void parseCommand(String com)
+{
+  String str1 = com.substring(1, 4);
+  String str2 = com.substring(5, 8);
+  String str3 = com.substring(9, 12);
+  String str4 = com.substring(13);
+  
+  PWM_A = str1.toInt();
+  PWM_B = str2.toInt();
+  PWM_C = str3.toInt();
+  PWM_D = str4.toInt();
+  
   analogWrite(PIN_A, PWM_A);
   analogWrite(PIN_B, PWM_B);
   analogWrite(PIN_C, PWM_C);
   analogWrite(PIN_D, PWM_D);
   
-  while (Serial.available())
-  { 
-    if (Serial.find("A"))
-    {
-      int val = Serial.parseInt();
-      PWM_A = constrain(val, 0, 255);
-    }
-    else break;
-    if (Serial.find("B"))
-    {
-      int val = Serial.parseInt();
-      PWM_B = constrain(val, 0, 255);
-    }
-    else break;
-    if (Serial.find("C"))
-    {
-      int val = Serial.parseInt();
-      PWM_C = constrain(val, 0, 255);
-    }
-    else break;
-    if (Serial.find("D"))
-    {
-      int val = Serial.parseInt();
-      PWM_D = constrain(val, 0, 255);
-    }
-    else break;
-  }
+  Serial.print(PWM_D);
 }
